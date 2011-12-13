f251f8d58d022a25179ca7b49e6c44469d25e39c
Adam Victor Nazareth Brandizzi <brandizzi@gmail.com>
2011-12-13 15:26:43 -0200

    Now packed with distutils

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='golem',
+      version='0.1.0',
+      description='Golem Unobtrusive Template',
+      author='Adam Victor Nazareth Brandizzi',
+      author_email='brandizzi@gmail.com',
+      py_modules=['golem']
+     )
