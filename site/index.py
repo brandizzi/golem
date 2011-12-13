678fc44d90bcafe4bb2e45b4b363b69fea9340d0
Adam Victor Nazareth Brandizzi <brandizzi@gmail.com>
2011-12-13 16:24:23 -0200

    Now the example site is working

---
+++ site/index.py
@@ -0,0 +1,27 @@
+import os.path
+
+import cherrypy
+
+import golem
+
+site_dir = os.path.split(os.path.realpath(__file__))[0]
+template_dir = os.path.join(site_dir, 'templates')
+index_template_path = os.path.join(template_dir, 'index.html')
+index_template = open(index_template_path).read()
+
+class GolemSite(object):
+    def index(self):
+        animator = golem.Animator(index_template)
+
+        animator.fill('a#downladPage', 
+                href='https://bitbucket.org/brandizzi/golem/downloads')
+        animator.fill('#version', '0.1.0')
+
+        doctest_file = open(os.path.join(site_dir, os.path.pardir, 'golem.txt'))
+        doctest = doctest_file.read()
+        animator.fill("code.doctest", doctest)
+        
+        return animator.result()
+    index.exposed = True
+
+cherrypy.quickstart(GolemSite())
