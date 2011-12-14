42883ee66611c467992cebb7fb0ffb1b7952ced7
Adam Victor Nazareth Brandizzi <brandizzi@gmail.com>
2011-12-14 11:13:11 -0200

    Improving distutils config

--- setup.py
+++ setup.py
@@ -3,7 +3,15 @@ from distutils.core import setup
 setup(name='golem',
       version='0.1.1dev',
       description='Golem Unobtrusive Template',
+      long_description="""
+      Golem is an experimental, simple template engine for template animation
+      (or unobtrusive server scripting). Golem allows developers to gelerate
+      HTML interface for data based simple HTML mockups. Golem templates
+      has no new tempalte syntax, no special XML elements, just plain HTML.
+      """,
       author='Adam Victor Nazareth Brandizzi',
       author_email='brandizzi@gmail.com',
-      py_modules=['golem']
+      url = 'http://bitbucket.org/brandizzi/golem',
+      py_modules=['golem'],
+      requires=['lxml (>=2.3)'],
      )
