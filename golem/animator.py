3a503d7d066648f02e25cc78ac016319ef3f908c
Adam Victor Nazareth Brandizzi <brandizzi@gmail.com>
2015-12-07 05:48:52 -0300

    #6 A naive, inefficient implementation.
    This is just a way to get tests passing. It is going to be improved soon.

--- golem/animator.py
+++ golem/animator.py
@@ -17,5 +17,15 @@ class Animator(object):
     def find(self, class_=None):
         return self.class_map[class_]
 
+    def take(self, *args, **kwargs):
+        elements = self.find(*args, **kwargs)
+
+        for e in self.document.iter():
+            for f in elements:
+                if f in e:
+                    e.remove(f)
+
+        return elements
+
     def tostring(self):
         return self.parser.tostring(self.document)
