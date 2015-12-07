57dc27cf8491935db5b949ef82f093b3f59a82c2
Adam Victor Nazareth Brandizzi <brandizzi@gmail.com>
2015-12-07 05:32:37 -0300

    #6 Test for "tostring()" method.
    We need this method for testing "take()" more naturally.

--- golem/test/animator.py
+++ golem/test/animator.py
@@ -24,6 +24,36 @@ class TestAnimator(unittest.TestCase):
         self.assertEquals('Text 1', divs[0].text)
         self.assertEquals('Text 3', divs[1].text)
 
+    def test_tostring(self):
+        """
+        Animator should have a ``tostring()`` method to return its document
+        as a string again.
+        """
+        document = """
+        <html>
+            <body>
+                <div class="c1">Text 1</div>
+                <div class="c2">Text 2</div>
+                <div class="c1">Text 3</div>
+            </body>
+        </html>
+        """
+        animator = Animator(document)
+        divs = animator.find(class_='c1')
+
+        divs[0].text = 'UPDATED'
+
+        result = """
+        <html>
+            <body>
+                <div class="c1">UPDATED</div>
+                <div class="c2">Text 2</div>
+                <div class="c1">Text 3</div>
+            </body>
+        </html>
+        """.strip()
+        self.assertEquals(result, animator.tostring())
+
 import inelegant.finder
 
 load_tests = inelegant.finder.TestFinder(__name__).load_tests
